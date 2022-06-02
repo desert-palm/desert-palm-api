fb7f3b203ee5b90216f612b461fea805a0588015
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-06-02 19:01:00 -0400

    Renamed product service in resolver

--- src/products/products.resolver.ts
+++ src/products/products.resolver.ts
@@ -5,20 +5,20 @@ import { ProductsService } from "./products.service";
 
 @Resolver((_of: Product) => Product)
 export class ProductsResolver {
-  constructor(private productsService: ProductsService) {}
+  constructor(private service: ProductsService) {}
 
   @Query((_returns) => Product)
   async getProduct(@Args("id", { type: () => Int }) id: number) {
-    return this.productsService.getProduct(id);
+    return this.service.getProduct(id);
   }
 
   @Query((_returns) => [Product])
   async getProducts() {
-    return this.productsService.getProducts();
+    return this.service.getProducts();
   }
 
   @Mutation(() => Product)
   async createProduct(@Args("productData") productData: ProductInput) {
-    return this.productsService.createProduct(productData);
+    return this.service.createProduct(productData);
   }
 }
